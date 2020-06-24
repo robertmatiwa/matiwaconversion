f5ab39e272d3bcc87cc7769d32876ccf0de2f62f
Admin <Admin@192.168.0.101>
2020-06-24 14:55:32 +0200

    Initial commit of project

---
+++ matiwa/src/test/java/com/conversion/matiwa/MatiwaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conversion.matiwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatiwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
